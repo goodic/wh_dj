e11724453606512ec676f5faa192370ad183cb2f
Sergey Gudik <sergey.goodic@gmail.com>
2022-06-29 23:18:45 +0300

    db hw catalog

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=254, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=1)
+    image = models.TextField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Phone, self).save(*args, **kwargs)
