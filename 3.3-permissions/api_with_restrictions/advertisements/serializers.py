22cdfb4d34c40904fb23273132ef3b025fbc8645
Sergey Gudik <sergey.goodic@gmail.com>
2022-07-31 21:00:26 +0300

    base home work

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from advertisements.models import Advertisement
 
+
+open_adv_limit = 10
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer для пользователя."""
@@ -39,7 +42,11 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
-
+        validated_stat = 'OPEN'
         # TODO: добавьте требуемую валидацию
-
+        if data.get('status', 'OPEN') == validated_stat:
+            open_adv_count = len(Advertisement.objects.filter(status=validated_stat, creator=self.context["request"].user))
+            print(open_adv_count)
+            if open_adv_count >= open_adv_limit:
+                raise ValidationError(f'Можно разместить не более {open_adv_limit} активных объявлений')
         return data
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,9 +1,24 @@
+from django_filters import rest_framework as filters, DateFromToRangeFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+
+
+class CreatedAtFilter(filters.FilterSet):
+    created_at = DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at']
+
 
 class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = CreatedAtFilter
+    filterset_fields = ['status']
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
